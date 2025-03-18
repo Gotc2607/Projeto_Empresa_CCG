a694e3161496a60a166aad033002a430c97e7880
Giovani Coelho <giovanicoelho2005@gmail.com>
2025-03-18 12:06:53 -0300

    melhorando o estilo da pagina inicial

---
+++ CCG/Pagina_inicial/app/statics/js/scripty.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - 50,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
